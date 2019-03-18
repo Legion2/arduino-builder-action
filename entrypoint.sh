76f46f1b72138494a52dc231bb1f2e763ce4e972
Legion2 <leon.k97@gmx.de>
2019-03-18 23:50:27 +0100

    fixed SKETCH_PATH option did not prevent SKETCH_DIRECTORY_PATH usage

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,18 +20,18 @@ if [ ! -z "$SKETCH_PATH" ]; then
     else
         ./arduino-builder $@ "$SKETCH_PATH"
     fi
-fi
-
-if [ -z "$SKETCH_DIRECTORY_PATH" ]; then
-    SKETCH_DIRECTORY_PATH="${GITHUB_WORKSPACE}/examples/"
-fi
-
-cd /opt/arduino
-for sketch in `find "${SKETCH_DIRECTORY_PATH}" -name '*.ino'`
-do
-    if [ -z "$1" ]; then
-        ./arduino-builder -hardware ./hardware -tools ./hardware/tools/avr -tools ./tools-builder -libraries ./libraries -libraries $LIBRARIES_PATH -libraries $GITHUB_WORKSPACE/../ -fqbn $BOARD_NAME $sketch
-    else
-        ./arduino-builder $@ $sketch
+else
+    if [ -z "$SKETCH_DIRECTORY_PATH" ]; then
+        SKETCH_DIRECTORY_PATH="${GITHUB_WORKSPACE}/examples/"
     fi
-done
+
+    cd /opt/arduino
+    for sketch in `find "${SKETCH_DIRECTORY_PATH}" -name '*.ino'`
+    do
+        if [ -z "$1" ]; then
+            ./arduino-builder -hardware ./hardware -tools ./hardware/tools/avr -tools ./tools-builder -libraries ./libraries -libraries $LIBRARIES_PATH -libraries $GITHUB_WORKSPACE/../ -fqbn $BOARD_NAME $sketch
+        else
+            ./arduino-builder $@ $sketch
+        fi
+    done
+fi
